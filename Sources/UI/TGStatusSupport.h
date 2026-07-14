72188d0b6d97c66426c1d3bdca36114379adcaa5
Yura Menschikov <yura.menschikov@icloud.com>
2026-07-14 16:08:30 +0300

    Extract download folder helpers

--- Sources/UI/TGStatusSupport.h
+++ Sources/UI/TGStatusSupport.h
@@ -3,6 +3,10 @@
 BOOL TGVersionStringIsNewer(NSString *candidate, NSString *current);
 BOOL TGUserDefaultBoolWithDefault(NSString *key, BOOL defaultValue);
 BOOL TGStatusErrorLooksOffline(NSString *message);
+NSString *TGDefaultDownloadFolderPath(void);
+NSString *TGConfiguredDownloadFolderPath(void);
+NSString *TGDisplayPathForDownloadFolder(NSString *path);
+void TGSetConfiguredDownloadFolderPath(NSString *path);
 NSString *TGCurrentYearString(void);
 NSString *TGLogTimestampString(void);
 NSString *TGLogSectionForDetail(NSString *detail);
