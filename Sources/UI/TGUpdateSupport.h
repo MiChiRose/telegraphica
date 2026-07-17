ba620bd89396aec411d0215d0827994adfd9099e
Yura Menschikov <yura.menschikov@icloud.com>
2026-07-17 14:57:33 +0300

    Add Cloudflare-backed update downloader

--- Sources/UI/TGUpdateSupport.h
+++ Sources/UI/TGUpdateSupport.h
@@ -1,8 +1,13 @@
 #import <Foundation/Foundation.h>
 
 NSString *TGCurrentApplicationVersionString(void);
+NSString *TGUpdateManifestURLString(void);
 NSString *TGUpdateProjectReleasesURLString(void);
 NSURL *TGUpdateProjectReleasesURL(void);
 NSString *TGUpdateCheckUserAgentString(void);
 NSString *TGGitHubErrorMessageFromData(NSData *data, NSString *fallback);
 NSDictionary *TGLatestGitHubReleaseInfoWithError(NSError **error);
+NSDictionary *TGLatestUpdateInfoWithError(NSError **error);
+NSString *TGUpdateDownloadURLStringFromInfo(NSDictionary *info);
+NSString *TGUpdateDownloadFileNameFromInfo(NSDictionary *info);
+BOOL TGUpdateFileMatchesSHA256(NSString *path, NSString *expectedSHA256, NSError **error);
